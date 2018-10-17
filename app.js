7c9b3f8c7cd8a0f6ca080e852a8c4a74a4465eea
Taylor Ward <takai.tech2144@gmail.com>
2018-10-16 17:21:41 -0700

    Rendered images to DOM

--- app.js
+++ app.js
@@ -4,13 +4,26 @@ function getDogImages() {
     var url = `https://dog.ceo/api/breeds/image/random/${number}`;
     fetch(url)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson))
+    .then(responseJson => displayImages(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayImages(responseJson) {
+    console.log(responseJson);
+    for (let i = 0; i < responseJson.message.length; i++) {
+    $('.results').append(`<img src="${responseJson.message[i]}" class="results-img">`)
+    $('.results').removeClass('hidden');
+    }
+}
+
+function deleteImages() {
+    $("img").remove();
 }
 
 function watchForm() {
     $("form").submit(event => {
         event.preventDefault();
+        deleteImages();
         getDogImages();
     });
 }
